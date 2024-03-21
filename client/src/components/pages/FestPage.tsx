8b8074ea827dab827645e788c02bd345272e9569
mr.advert <mr.advert@yandex.ru>
2024-03-21 17:56:35 +0300

    Fest page fixed

--- client/src/components/pages/FestPage.tsx
+++ client/src/components/pages/FestPage.tsx
@@ -5,15 +5,9 @@ import getAllFestsThunk from '../../redux/slices/fest/thunk';
 
 export default function FestPage(): JSX.Element {
   const fests = useAppSelector((state) => state.festivals.fests);
-
- 
-
-  
-
   return (
     <div>
       <h1>FestPage</h1>
-      {/* <OneFest /> */}
       {fests.map((fest) => (
         <OneFest fest={fest} key={fest.id} />
       ))}
