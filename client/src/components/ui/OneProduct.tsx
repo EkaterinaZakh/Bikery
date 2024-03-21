7e0a8ddbb7f56a6abc0efb45769aa18939534b32
EkaterinaZakh <zakharovaea95@yandex.ru>
2024-03-21 16:12:04 +0300

    components cards

--- client/src/components/ui/OneProduct.tsx
+++ client/src/components/ui/OneProduct.tsx
@@ -1,5 +1,31 @@
 import React from 'react';
+import { Box, Button, Card, CardContent, CardMedia, IconButton, Typography } from '@mui/material';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 
 export default function OneProduct(): JSX.Element {
-  return <div>OneProduct</div>;
+  return (
+    <Card sx={{ display: 'flex' }}>
+      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+        <CardContent sx={{ flex: '1 0 auto' }}>
+          <Typography component="div" variant="h5">
+            Шлем
+          </Typography>
+          <Typography variant="subtitle1" color="text.secondary" component="div">
+            10 000 р.
+          </Typography>
+          <IconButton aria-label="add to favorites">
+            <FavoriteIcon />
+          </IconButton>
+          <Button href="#text-buttons">Купить</Button>
+        </CardContent>
+      </Box>
+
+      <CardMedia
+        component="img"
+        sx={{ width: 151 }}
+        image="https://images.unsplash.com/photo-1610900656436-1baa9fbe8d05?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt=""
+      />
+    </Card>
+  );
 }
