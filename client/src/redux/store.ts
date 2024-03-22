9c9e71d2509cb3fc53c0a1d5b46fc41105b6b71a
mr.advert <mr.advert@yandex.ru>
2024-03-22 11:56:49 +0300

    products mapping added

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,16 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
-import authReducer from './slices/auth/slice'
+import authReducer from './slices/auth/slice';
 import festReducer from './slices/fest/slice';
+import prodReducer from './slices/prod/slice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     festivals: festReducer,
+    products: prodReducer,
   },
 });
-
-
-
 
 export type StoreT = typeof store;
 // Infer the `RootState` and `AppDispatch` types from the store itself
