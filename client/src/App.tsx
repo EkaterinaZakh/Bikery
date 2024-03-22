4d48262ef23db855ee530b1bd79ba3c165c105a9
Grimaliak <tirisfallen@gmail.com>
2024-03-22 16:48:38 +0300

    zacenite etot shedewr

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,9 +5,9 @@ import MainPage from './components/pages/MainPage';
 import Root from './components/Root';
 import FestPage from './components/pages/FestPage';
 import { useAppDispatch, useAppSelector } from './redux/hooks';
-import LoginPage from './components/pages/auth/LoginPage';
-import SignupPage from './components/pages/auth/SignupPage';
-import PrivateRoute from './components/HOC/PrivateRoute';
+// import LoginPage from './components/pages/auth/LoginPage';
+// import SignupPage from './components/pages/auth/SignupPage';
+// import PrivateRoute from './components/HOC/PrivateRoute';
 import { refreshAuth } from './redux/slices/auth/thunks';
 import Loader from './components/HOC/Loader';
 import getAllCatsThunk from './redux/slices/cats/thunk';
@@ -44,13 +44,13 @@ function App(): JSX.Element {
         { path: '/', element: <MainPage /> },
         { path: '/fests', element: <FestPage /> },
         { path: '/shop', element: <ShopPage />},
-        {
-          element: <PrivateRoute isAllowed={status === 'guest'} redirect="/" />,
-          children: [
-            { path: '/login', element: <LoginPage /> },
-            { path: '/signup', element: <SignupPage /> },
-          ],
-        },
+        // {
+        //   element: <PrivateRoute isAllowed={status === 'guest'} redirect="/" />,
+        //   children: [
+        //     { path: '/login', element: <LoginPage /> },
+        //     { path: '/signup', element: <SignupPage /> },
+        //   ],
+        // },
       ],
     },
   ]);
---
+++ client/src/components/ui/BaseModal.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import  Modal from "@mui/material/Modal" 
+import Box from '@mui/material/Box';
+
+type BaseModalType = {
+    children: JSX.Element;
+    onClose: () => void;
+    open: boolean
+}
+
+const modalStyle = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  };
+
+export default function BaseModal({children, onClose, open}: BaseModalType): JSX.Element {
+  return (
+    <Modal
+    open={open}
+    onClose={onClose}
+    aria-labelledby='modal-modal-title'
+    aria-describedby='modal-modal-description'
+    >
+        <Box sx={modalStyle}>
+        {children}
+        </Box>
+    </Modal>
+  )
+}
