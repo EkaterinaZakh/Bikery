538c71689fd8e38348d9b073bad00b1dd88f91fd
Romanus91 <romanov777123@gmail.com>
2024-03-28 11:12:03 +0300

    Release Style for Festpage and form

--- client/src/components/pages/MainPage.tsx
+++ client/src/components/pages/MainPage.tsx
@@ -2,8 +2,6 @@ import { Box, Typography } from '@mui/material';
 import React from 'react';
 
 export default function MainPage(): JSX.Element {
-
-  
   return (
     <Box
       sx={{
@@ -26,16 +24,28 @@ export default function MainPage(): JSX.Element {
           color: 'rgba(33, 2, 2, 1)',
         }}
       >
-        <Typography variant="body2" sx={{ width: '50%', fontSize: '18px', marginRight: '50px' }}>
-          <strong>Bikery</strong> - портал, где свобода не просто слово, а образ жизни. Здесь каждый
-          может почувствовать настоящую скорость, ощутить дух движения и стать частью нашего
-          сильного сообщества байкеров. Мы поощряем бунтарство против обыденности и вместе создаем
-          эпические истории на дорогах. Присоединяйтесь к нам!
+        <Typography
+          variant="body2"
+          sx={{
+            width: '50%',
+            fontSize: '25px',
+            marginRight: '50px',
+            backgroundColor: 'rgba(255, 255, 255, 0.7)',
+            borderRadius: '10px', // Округление углов
+            padding: '10px', // Поля внутри прямоугольника
+          }}
+        >
+          <strong>
+            Bikery - портал, где свобода не просто слово, а образ жизни. Здесь каждый может
+            почувствовать настоящую скорость, ощутить дух движения и стать частью нашего сильного
+            сообщества байкеров. Мы поощряем бунтарство против обыденности и вместе создаем
+            эпические истории на дорогах. Присоединяйтесь к нам!
+          </strong>
         </Typography>
         <Box
           sx={{
             width: '50%',
-            height: '90px',
+            height: '80px',
             backgroundColor: 'rgba(255, 255, 255, 0.7)',
             borderRadius: '10px', // Округление углов
             padding: '10px', // Поля внутри прямоугольника
@@ -44,7 +54,7 @@ export default function MainPage(): JSX.Element {
           }}
         >
           <Typography className="text" variant="h5">
-              <strong>Знать дорогу и проехать по ней - не одно и то же!</strong>
+            <strong>Знать дорогу и проехать по ней - не одно и то же!</strong>
           </Typography>
         </Box>
       </Box>
--- client/src/components/ui/BaseModal.tsx
+++ client/src/components/ui/BaseModal.tsx
@@ -1,30 +1,30 @@
-import React from 'react'
-import  Modal from "@mui/material/Modal" 
+import React from 'react';
+import Modal from '@mui/material/Modal';
 import Box from '@mui/material/Box';
 
 type BaseModalType = {
-    children: JSX.Element;
-    onClose: () => void;
-    open: boolean
-}
+  children: JSX.Element;
+  onClose: () => void;
+  open: boolean;
+};
 
 const modalStyle = {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  };
+  position: 'fixed',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  // background: 'rgba(255, 255, 255, 0.9)'
+};
 
-export default function BaseModal({children, onClose, open}: BaseModalType): JSX.Element {
+export default function BaseModal({ children, onClose, open }: BaseModalType): JSX.Element {
   return (
     <Modal
-    open={open}
-    onClose={onClose}
-    aria-labelledby='modal-modal-title'
-    aria-describedby='modal-modal-description'
+      open={open}
+      onClose={onClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
     >
-        <Box sx={modalStyle}>
-        {children}
-        </Box>
+      <Box sx={modalStyle}>{children}</Box>
     </Modal>
-  )
+  );
 }
